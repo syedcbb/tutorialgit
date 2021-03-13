b564da7d02380146c6a8e95cd6889283a94dcb2a
syed huss <syedcbb@gmail.com>
2021-03-12 19:21:16 -0600

    tryna merge with master

--- src/main/java/com/example/Greeter.java
+++ src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello How are you my g i am editing this hahaha, and i am adding to the new branch devolop from the secondary user!!!! %s!", someone);
+    return String.format("Hello How are you my g i am editing this hahaha, and i am adding to the new branch devolop from the secondary user now im tryingto merge to master!!!! %s!", someone);
   }
 }
